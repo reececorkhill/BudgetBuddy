bed0d79cc5bfb51c28ee4147425ef6c3727cdbd4
Reece <reece@smartit-webdevelopment.com>
2024-03-13 22:02:23 +0000

    Added Transactions component.

---
+++ src/components/Transactions.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Transactions = () => {
+    return (
+        <div>
+            <ul class="list-group">
+                <p>Bill Name / + or - Amount</p>
+                <li class="list-group-item">Takeaway -£25.00</li>
+                <li class="list-group-item">Salary +£2150</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Transactions;
