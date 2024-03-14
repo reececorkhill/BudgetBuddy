731b2894531f8fb07178524c497e844d744ca90f
Reece <reece@smartit-webdevelopment.com>
2024-03-14 19:28:36 +0000

    Updated Transaction component and added bootstrap import.

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import Badge from 'react-bootstrap/Badge';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 const Transactions = () => {
     return (
         <div>
-            <ul class="list-group">
-                <p>Bill Name / + or - Amount</p>
-                <li class="list-group-item">Takeaway -£25.00</li>
-                <li class="list-group-item">Salary +£2150</li>
-            </ul>
+            <ListGroup as="ol" numbered>
+                <ListGroup.Item as="li" className="d-flex justify-content-between align-items-start">
+                    <div className="ms-2 me-auto">
+                        <div className="fw-bold">Monthly Salary</div> {/* <-- PLACEHOLDER TEXT: This will be the Transaction Name - from localstorage. */}
+                    </div>
+                    <Badge bg="primary" pill>+£2500.00</Badge> {/* <-- PLACEHOLDER TEXT: This will be the Transaction Amount - from localstorage. */}
+                </ListGroup.Item>
+                <ListGroup.Item as="li" className="d-flex justify-content-between align-items-start" >
+                    <div className="ms-2 me-auto">
+                        <div className="fw-bold">Takeaway</div>
+                    </div>
+                    <Badge bg="primary" pill>-£25.00</Badge>
+                </ListGroup.Item>
+            </ListGroup>
         </div>
     );
 }
