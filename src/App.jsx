1358960164388a5ae9533f061771d63fa87dfbf5
sridharaniruddha <152184230+sridharaniruddha@users.noreply.github.com>
2024-03-18 15:33:15 +0000

    testing menu components

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,22 @@ import './App.css'
 import Message from './components/Message.jsx';
 import AddTransaction from './components/AddTransaction.jsx';
 import Transactions from './components/Transactions.jsx';
+import Interface from './components/Interface.jsx';
+
+// const App:React.FC = () => (
+//   <div className="App">
+//     <Button type="primary">Button</Button>
+//   </div>
+// );
 
 function App() {
 
   return (
     <>
+
+    <div className="Interface-section">
+      <Interface />
+    </div>
       <div className="welcome-section">
         <Message /> {/* This is the welcome message. */}
       </div>
@@ -23,4 +34,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
