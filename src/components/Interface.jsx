56b8f0c3eb564ce348c38995750dc394556d8f59
sridharaniruddha <152184230+sridharaniruddha@users.noreply.github.com>
2024-03-18 15:49:23 +0000

    creating float button

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -2,66 +2,100 @@ import React, { useState } from 'react';
 import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
 import { Menu } from 'antd';
 import { Button } from 'antd';
+import { CommentOutlined, CustomerServiceOutlined, PlusCircleFilled, } from '@ant-design/icons';
 
-function getItem(label, key, icon, children, type) {
-    return {
-        key,
-        icon,
-        children,
-        label,
-        type,
-    };
-}
-const items = [
-    getItem('Navigation One', 'sub1', <MailOutlined />, [
-        getItem('Option 1', '1'),
-        getItem('Option 2', '2'),
-        getItem('Option 3', '3'),
-        getItem('Option 4', '4'),
-    ]),
-    getItem('Navigation Two', 'sub2', <AppstoreOutlined />, [
-        getItem('Option 5', '5'),
-        getItem('Option 6', '6'),
-        getItem('Submenu', 'sub3', null, [getItem('Option 7', '7'), getItem('Option 8', '8')]),
-    ]),
-    getItem('Navigation Three', 'sub4', <SettingOutlined />, [
-        getItem('Option 9', '9'),
-        getItem('Option 10', '10'),
-        getItem('Option 11', '11'),
-        getItem('Option 12', '12'),
-    ]),
-];
+import { FloatButton, Switch } from 'antd';
 
-// submenu keys of first level
-const rootSubmenuKeys = ['sub1', 'sub2', 'sub4'];
-const Design = () => {
-    const [openKeys, setOpenKeys] = useState(['sub1']);
-    const onOpenChange = (keys) => {
-        const latestOpenKey = keys.find((key) => openKeys.indexOf(key) === -1);
-        if (latestOpenKey && rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
-            setOpenKeys(keys);
-        } else {
-            setOpenKeys(latestOpenKey ? [latestOpenKey] : []);
-        }
-    };
-    return (
-        <div>
-            <Menu
-                mode="inline"
-                openKeys={openKeys}
-                onOpenChange={onOpenChange}
-                style={{
-                    width: 256,
-                }}
-                items={items}
-            />
-            <Button type="primary" className='myBtn'>Primary Button</Button>
-        </div>
+// function getItem(label, key, icon, children, type) {
+//     return {
+//         key,
+//         icon,
+//         children,
+//         label,
+//         type,
+//     };
+// }
+// const items = [
+//     getItem('Navigation One', 'sub1', <MailOutlined />, [
+//         getItem('Option 1', '1'),
+//         getItem('Option 2', '2'),
+//         getItem('Option 3', '3'),
+//         getItem('Option 4', '4'),
+//     ]),
+//     getItem('Navigation Two', 'sub2', <AppstoreOutlined />, [
+//         getItem('Option 5', '5'),
+//         getItem('Option 6', '6'),
+//         getItem('Submenu', 'sub3', null, [getItem('Option 7', '7'), getItem('Option 8', '8')]),
+//     ]),
+//     getItem('Navigation Three', 'sub4', <SettingOutlined />, [
+//         getItem('Option 9', '9'),
+//         getItem('Option 10', '10'),
+//         getItem('Option 11', '11'),
+//         getItem('Option 12', '12'),
+//     ]),
+// ];
 
-    );
+// // submenu keys of first level
+// const rootSubmenuKeys = ['sub1', 'sub2', 'sub4'];
+// const Design = () => {
+//     const [openKeys, setOpenKeys] = useState(['sub1']);
+//     const onOpenChange = (keys) => {
+//         const latestOpenKey = keys.find((key) => openKeys.indexOf(key) === -1);
+//         if (latestOpenKey && rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
+//             setOpenKeys(keys);
+//         } else {
+//             setOpenKeys(latestOpenKey ? [latestOpenKey] : []);
+//         }
+//     };
+//     return (
+//         <div>
+//             <Menu
+//                 mode="inline"
+//                 openKeys={openKeys}
+//                 onOpenChange={onOpenChange}
+//                 style={{
+//                     width: 256,
+//                 }}
+//                 items={items}
+//             />
+//             <Button type="primary" className='myBtn'>Primary Button</Button>
+//         </div>
+
+//     );
+// };
+
+
+const App = () => {
+  const [open, setOpen] = useState(true);
+  const onChange = (checked) => {
+    setOpen(checked);
+  };
+  return (
+    <>
+      <FloatButton.Group
+        open={open}
+        trigger="click"
+        style={{
+          right: 24,
+        }}
+        icon={<CustomerServiceOutlined />}
+      >
+        <FloatButton />
+        <FloatButton icon={<CommentOutlined />} />
+      </FloatButton.Group>
+      <Switch
+        onChange={onChange}
+        checked={open}
+        style={{
+          margin: 16,
+        }}
+      />
+    </>
+  );
 };
 
 
 
-export default Design;
+
+export default App;
 
